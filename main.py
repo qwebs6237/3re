91ac9addb922dafd90bc593031904c92dc43d50a
qwebs6237 <grigoraenarek@gmail.com>
2025-04-27 17:09:20 +0300

    Update main.py
    переход к OP

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import random 
-def wer(a, b):
-    j = random.randint(a, b)
-    return j
+
+
+class Randomizer:
+    def __init__(self, x, y):
+        self.b1 = x
+        self.b2 = y
+        self.v = self._get_random_v()
+
+    def _get_random_v(self):
+        return random.randint(self.b1 , self.b2)
+
+    def get_v(self):
+        return self.v
 
 a = int(input('Введите число '))
 b = int(input('Введите число '))
-res = wer(a, b)
-print('случайное число:', res)
+res = Randomizer(a, b)
+re = res.get_v()
+print('случайное число:', re)
